bf99bd1c48c3518864f7ba378aa9f9bc0f93124f
milk-stone <ywshero@naver.com>
2025-10-27 17:49:12 +0900

    feat(controller): Implement main game flow and application entry point

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -2,13 +2,34 @@ package racingcar.controller;
 
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
+import racingcar.domain.RacingGame;
+
+import java.util.List;
 
 public class RaceController {
-    private InputView inputView;
-    private OutputView outputView;
+    private final InputView inputView;
+    private final OutputView outputView;
 
     public RaceController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+    }
+
+    public void run() {
+        String carNamesInput = inputView.readCarNames();
+        String trialCountInput = inputView.readTrialCount();
+
+        RacingGame game = new RacingGame(carNamesInput, trialCountInput);
+
+        int trialCount = game.getTrialCount();
+
+        outputView.printRaceResultHeader();
+        for (int i = 0; i < trialCount; i++) {
+            game.runRound();
+            outputView.printRoundResult(game.getCars());
+        }
+
+        List<String> winners = game.getWinnerNames();
+        outputView.printWinners(winners);
     }
 }
